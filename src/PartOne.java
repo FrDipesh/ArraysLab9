4c79e0911d5455971dd48b05b5deaca79e59d514
618989 <poudel.618989@swl.k12.oh.us>
2025-11-11 08:56:05 -0500

    Lab9

--- src/PartOne.java
+++ src/PartOne.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class PartOne {
+    public static void main(String[] args) {
+        int [] dataPoints = new int [100];
+        Random random = new Random();
+
+        //Assigning random num 1-10 to the array
+        for (int i = 0; i < dataPoints.length; i++){
+            dataPoints[i] = random.nextInt(100) + 1;
+        }
+    }
+
 }
